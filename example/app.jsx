13ecbe48b4bb6146cfa38b79d1b924604b97ff64
Kevin Mees <kev.mees@gmail.com>
2015-09-04 13:17:42 +0200

    :memo: Update example

--- example/app.jsx
+++ example/app.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { Label } from '../src'
+import { Label, TextField } from '../src'
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <h2>Label</h2>
         <Label>Default Label</Label>
         <Label required={true}>Required Label</Label>
         <Label disabled={true}>Disabled Label</Label>
+
+        <h2>TextField</h2>
+        <TextField label="Simple" />
+        <TextField label={
+          <Label required={true}>Complex Label</Label>
+        } />
+        <TextField label="With Description" description="Lorem ipsum sit dolor amet." />
+        <TextField label="Multiline" multiline={true} />
+        <TextField label="Placeholder" placeholder={true} />
+        <TextField label="Underlined" underlined={true} />
+        <TextField label="Required" required={true} />
       </div>
     )
   }
-
 }
 
-React.render(<App />, document.getElementById('main'))
+ReactDOM.render(<App />, document.getElementById('main'))
