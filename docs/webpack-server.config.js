0c4612d820836d92ca3c79d462ced3c34998be53
Kevin Mees <kev.mees@gmail.com>
2015-09-07 16:44:57 +0200

    Add webpack config for build

--- docs/webpack-server.config.js
+++ docs/webpack-server.config.js
@@ -1,72 +1,20 @@
 /* eslint-disable */
 
 var webpack = require('webpack');
-var path = require('path');
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var config = require('./webpack.config')
 
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-
-var config = {
-  //Entry point to the project
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/src/app/app.jsx')
-  ],
-  //Webpack config options on how to obtain modules
-  resolve: {
-    //When requiring, you don't need to add these extensions
-    extensions: ["", ".js", ".jsx"],
-    alias: {
-      'react-fabric': path.resolve(__dirname, '../src'),
-    },
-    //Modules will be searched for in these directories
-    modulesDirectories: [
-      "node_modules",
-      path.resolve(__dirname, '../src'),
-      path.resolve(__dirname, '../node_modules')
-    ],
-  },
-  //Configuration for dev server
-  devServer: {
-    contentBase: '',
-    devtool: 'eval',
-    hot: true,
-    inline: true,
-    port: 3000
-  },
+config.entry.unshift('webpack/hot/only-dev-server')
+config.entry.unshift('webpack/hot/dev-server')
+config.devServer = {
+  contentBase: '',
   devtool: 'eval',
-  //Output file config
-  output: {
-    filename: 'app.js'  //Name of output file
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    //Used to include index.html in build folder
-    new HtmlWebpackPlugin({
-        inject: false,
-        template: path.join(__dirname, '/src/index.html')
-    }),
-    //Allows error warninggs but does not stop compiling. Will remove when eslint is added
-    new webpack.NoErrorsPlugin(),
-  ],
-  module: {
-    //Allow loading of non-es5 js files.
-    loaders: [
-      {
-        test: /\.jsx?$/, //All .js and .jsx files
-        loaders: ['react-hot','babel-loader?stage=0'], //react-hot is like browser sync and babel loads jsx and es6-7
-        include: [__dirname, path.resolve(__dirname, '../src')], //include these files
-        exclude: [nodeModulesPath]  //exclude node_modules so that they are not all compiled
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!raw-loader',
-        include: [path.resolve(nodeModulesPath, 'Fabric')]
-      }
-    ]
-  },
+  hot: true,
+  inline: true,
+  port: 3000
 };
+config.devtool = 'eval';
+config.module.loaders[0].loaders.unshift('react-hot');
+config.plugins.unshift(new webpack.HotModuleReplacementPlugin())
 
 module.exports = config;
---
+++ docs/webpack.config.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+
+var webpack = require('webpack');
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+var buildPath = path.resolve(__dirname, 'build');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+var config = {
+  entry: [
+    path.join(__dirname, '/src/app/app.jsx')
+  ],
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+    alias: {
+      'react-fabric': path.resolve(__dirname, '../src'),
+    },
+    modulesDirectories: [
+      "node_modules",
+      path.resolve(__dirname, '../src'),
+      path.resolve(__dirname, '../node_modules'),
+    ],
+  },
+  devtool: 'source-map',
+  output: {
+    path: buildPath,
+    filename: 'app.js'
+  },
+  plugins: [
+    //Used to include index.html in build folder
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+        inject: false,
+        template: path.join(__dirname, '/src/index.html')
+    }),
+    new webpack.NoErrorsPlugin(),
+    new TransferWebpackPlugin([
+      {from: 'css'},
+    ], path.resolve(__dirname,"src"))
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        include: [path.resolve(__dirname, "../src")],
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader?stage=0'],
+        include: [__dirname, path.resolve(__dirname, '../src')],
+        exclude: [nodeModulesPath]
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!raw-loader',
+        include: [path.resolve(nodeModulesPath, 'Fabric')]
+      }
+    ]
+  },
+  eslint: {
+    configFile: '../.eslintrc'
+  }
+};
+
+module.exports = config;
