334c2bb4da64fc885b0988ca2e2808d7d045b2cf
Kevin Mees <kev.mees@gmail.com>
2015-09-09 00:14:48 +0200

    Add state handling to SearchBox component

---
+++ src/SearchBox.jsx
@@ -0,0 +1,74 @@
+import React, { PropTypes} from 'react'
+import cx from 'classnames'
+
+export default class SearchBox extends React.Component  {
+  static displayName = 'SearchBox'
+  static propTypes = {
+    label: PropTypes.oneOf([
+      PropTypes.string,
+      PropTypes.element,
+    ]),
+  }
+
+  constructor() {
+    super()
+
+    this.state = {
+      isActive: false,
+      value: '',
+    }
+
+    this._onInputBlur = this._onInputBlur.bind(this)
+    this._onInputChange = this._onInputChange.bind(this)
+    this._onInputFocus = this._onInputFocus.bind(this)
+  }
+
+  _onInputBlur() {
+    this.setState({
+      ...this.state,
+      isActive: false,
+    })
+  }
+
+  _onInputChange(e) {
+    this.setState({
+      ...this.state,
+      value: e.target.value,
+    })
+  }
+
+  _onInputFocus() {
+    this.setState({
+      ...this.state,
+      isActive: true,
+    })
+  }
+
+  render() {
+    const {
+      label,
+    } = this.props
+
+    return (
+      <div className={cx(
+        'ms-SearchBox', {
+          'is-active': this.state.isActive,
+        }
+      )}>
+        <input className="ms-SearchBox-field"
+          onBlur={this._onInputBlur}
+          onChange={this._onInputChange}
+          onFocus={this._onInputFocus} />
+        <label className="ms-SearchBox-label" style={{
+          display: this.state.isActive || this.state.value ? 'none' : '',
+        }}>
+          <i className="ms-SearchBox-icon ms-Icon ms-Icon--search" />
+          {label}
+        </label>
+        <button className="ms-SearchBox-closeButton">
+          <i className="ms-Icon ms-Icon--x" />
+        </button>
+      </div>
+    )
+  }
+}
---
+++ test/SearchBox.spec.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import TestUtils, { Simulate } from 'react-addons-test-utils'
+
+import render from './util/render'
+
+import SearchBox from '../src/SearchBox'
+
+describe('SearchBox', () => {
+  it('has a display name', () => {
+    expect(SearchBox).to.have.property('displayName', 'SearchBox')
+  })
+
+  describe('[Presentation]', () => {
+    it('renders a wrapped <input />', () => {
+      var sut = render(
+        <SearchBox />
+      )
+
+      var [input] = sut.props.children
+
+      expect(sut).to.have.property('type', 'div')
+      expect(sut).to.have.deep.property('props.className', 'ms-SearchBox')
+
+      expect(input).to.have.property('type', 'input')
+      expect(input).to.have.deep.property('props.className', 'ms-SearchBox-field')
+    })
+
+    it('can have a label', () => {
+      var sut = render(
+        <SearchBox label="Foo" />
+      )
+
+      var [, label] = sut.props.children
+
+      expect(label).to.have.property('type', 'label')
+      expect(label).to.have.deep.property('props.className', 'ms-SearchBox-label')
+
+      expect(label.props.children[0]).to.have.deep.property('props.className', 'ms-SearchBox-icon ms-Icon ms-Icon--search')
+      expect(label.props.children[1]).to.equal('Foo')
+    })
+
+    it('has a close button', () => {
+      var sut = render(
+        <SearchBox />
+      )
+
+      var [, , closeBtn] = sut.props.children
+
+      expect(closeBtn).to.have.property('type', 'button')
+      expect(closeBtn).to.have.deep.property('props.className', 'ms-SearchBox-closeButton')
+
+      expect(closeBtn.props.children).to.have.deep.property('props.className', 'ms-Icon ms-Icon--x')
+    })
+  })
+
+  describe('[Functionality]', () => {
+    it('handles active state', () => {
+      var sut = TestUtils.renderIntoDocument(
+        <SearchBox />
+      )
+
+      var node = React.findDOMNode(sut)
+      var input = TestUtils.findRenderedDOMComponentWithTag(sut, 'input')
+      Simulate.focus(input)
+
+      expect(node.className).to.include('is-active')
+
+      Simulate.blur(input)
+
+      expect(node.className).to.not.include('is-active')
+    })
+
+    it('hides label when input has focus', () => {
+      var sut = TestUtils.renderIntoDocument(
+        <SearchBox label="Foo" />
+      )
+
+      var label = TestUtils.findRenderedDOMComponentWithTag(sut, 'label')
+      var input = TestUtils.findRenderedDOMComponentWithTag(sut, 'input')
+      Simulate.focus(input)
+
+      expect(label).to.have.deep.property('style.display', 'none')
+    })
+
+    it('hides label when input has a value', () => {
+      var sut = TestUtils.renderIntoDocument(
+        <SearchBox label="Foo" />
+      )
+
+      var label = TestUtils.findRenderedDOMComponentWithTag(sut, 'label')
+      var input = TestUtils.findRenderedDOMComponentWithTag(sut, 'input')
+      input.value = 'Bar'
+      Simulate.change(input)
+
+      expect(label).to.have.deep.property('style.display', 'none')
+    })
+  })
+})
