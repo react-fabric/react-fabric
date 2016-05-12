98494aaacea1c55aa93e8b4d7f2e5845a945eeb7
Kevin Mees <kev.mees@gmail.com>
2016-05-12 22:44:20 +0200

    feat(TextField): Implement TextField component

---
+++ src/TextField/TextField.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import cx from 'classnames'
+
+import Label from '../Label'
+import fabricComponent from '../fabricComponent.js'
+
+import style from './TextField.scss'
+
+const TextField = ({
+  active,
+  className,
+  defaultValue,
+  description,
+  disabled,
+  id,
+  label,
+  multiline,
+  name,
+  placeholder,
+  required,
+  underlined,
+  value,
+  ...props
+}) => {
+  const inputId = `TextField_${id || name || Date.now()}_input`
+  const input = React.cloneElement(multiline ? <textarea /> : <input />, {
+    ...props,
+    active,
+    defaultValue,
+    disabled,
+    id: inputId,
+    name,
+    styleName: 'ms-TextField-field',
+    value: typeof value === 'string' || value instanceof String ? value : defaultValue
+  })
+  const showLabel = placeholder ?
+    !active && !value : true
+
+  return (
+    <div data-fabric="TextField"
+      id={id}
+      className={className}
+      styleName={cx('ms-TextField', {
+        'ms-TextField--underlined': underlined,
+        'ms-TextField--multiline': multiline,
+        'ms-TextField--placeholder': placeholder,
+        'is-disabled': disabled,
+        'is-required': required
+      })}>
+      { label && showLabel && <Label styleName="ms-Label" htmlFor={inputId}>{label}</Label> }
+      { input }
+      { description && <span styleName="ms-TextField-description">
+        { description }
+      </span> }
+    </div>
+  )
+}
+TextField.propTypes = {
+  active: React.PropTypes.bool,
+  className: React.PropTypes.string,
+  defaultValue: React.PropTypes.string,
+  description: React.PropTypes.node,
+  disabled: React.PropTypes.bool,
+  id: React.PropTypes.string,
+  label: React.PropTypes.node,
+  multiline: React.PropTypes.bool,
+  name: React.PropTypes.string,
+  placeholder: React.PropTypes.bool,
+  required: React.PropTypes.bool,
+  underlined: React.PropTypes.bool,
+  value: React.PropTypes.string
+}
+TextField.defaultProps = {
+  active: false,
+  defaultValue: '',
+  disabled: false,
+  multine: false,
+  placeholder: false,
+  required: false,
+  underlined: false
+}
+
+export default fabricComponent(TextField, style)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export Grid from './Grid'
 export Icon from './Icon'
 export Link from './Link'
 export NavBar from './NavBar'
+export TextField from './TextField'
 export Toggle from './Toggle'
 
 export fabricComponent from './fabricComponent.js'
