53ed5a6de1e65467e4a41f5cfd5b3a523e2fd392
Kevin Mees <kev.mees@gmail.com>
2016-05-12 23:01:55 +0200

    feat(Label): implement Label component

---
+++ src/Label/Label.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import cx from 'classnames'
+import elementType from 'react-prop-types/lib/elementType'
+
+import invokeWhenNotDisabled from '../util/invokeWhenNotDisabled.js'
+import fabricComponent from '../fabricComponent.js'
+
+import style from './Label.scss'
+
+const Label = ({
+  children,
+  className,
+  componentClass: ComponentClass,
+  disabled,
+  htmlFor,
+  onClick,
+  required,
+  ...props
+  }) => (
+  <ComponentClass data-fabric="Label"
+    {...props}
+    htmlFor={htmlFor}
+    onClick={invokeWhenNotDisabled(disabled, onClick)}
+    className={className}
+    styleName={cx('ms-Label', {
+      'is-required': required,
+      'is-disabled': disabled
+    })}>
+    { children }
+  </ComponentClass>
+)
+Label.propTypes = {
+  children: React.PropTypes.node,
+  className: React.PropTypes.string,
+  componentClass: elementType,
+  disabled: React.PropTypes.bool,
+  htmlFor: React.PropTypes.string,
+  onClick: React.PropTypes.func,
+  required: React.PropTypes.bool
+}
+Label.defaultProps = {
+  componentClass: 'label',
+  disabled: false,
+  required: false
+}
+
+export default fabricComponent(Label, style)
