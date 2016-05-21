34852d589db91a31cb971330a143cdbeaf65efaa
Kevin Mees <kev.mees@gmail.com>
2016-05-21 19:08:16 +0200

    fix(Dialog): fix imports

--- src/Dialog/index.js
+++ src/Dialog/index.js
@@ -2,5 +2,5 @@ export default from './Dialog.js'
 
 export DialogAction from './DialogAction.js'
 export DialogButton from './DialogButton.js'
-export DialogHeader from './DialogHeader.js'
+export DialogTitle from './DialogTitle.js'
 export DialogSubtext from './DialogSubtext.js'
