e4847e47b621e2f90bd946a63ca6ce513d111617
Kevin Mees <kev.mees@gmail.com>
2016-07-05 21:24:30 +0200

    test(Dialog): more Dialog tests

--- src/Dialog/__tests__/Dialog_test.js
+++ src/Dialog/__tests__/Dialog_test.js
@@ -16,13 +16,37 @@ test('Dialog', t => {
 
 test('Dialog#render - simple', t => {
   const container = render(
-    <Dialog>Foo</Dialog>
+    <Dialog>
+      Foo
+    </Dialog>
   ).contents()
 
   t.assert(container.is('div.ms-Dialog', 'container'))
   t.assert(container.is('[data-fabric="Dialog"]'), 'data-fabric')
   t.equal(container.find('.ms-Dialog-title').text(), 'Dialog')
   t.equal(container.find('.ms-Dialog-content').text(), 'Foo')
+
+  t.end()
+})
+
+test('Dialog#render - full', t => {
+  const container = render(
+    <Dialog closeable hidden>
+      <Dialog.Title>My Dialog</Dialog.Title>
+      <Dialog.Subtext>Foo</Dialog.Subtext>
+      <Dialog.Action>Ok</Dialog.Action>
+    </Dialog>
+  ).contents()
+
+  t.assert(container.hasClass('ms-Dialog--close'))
+  t.assert(container.hasClass('ms-Dialog--hidden'))
+  t.equal(
+    container.find('.ms-Dialog-main .ms-Dialog-button.ms-Dialog-button--close').length, 1,
+    'close button'
+  )
+  t.equal(container.find('.ms-Dialog-title').text(), 'My Dialog')
+  t.equal(container.find('.ms-Dialog-content').text(), 'Foo')
+  t.equal(container.find('.ms-Dialog-actions .ms-Dialog-action').length, 1, 'action')
 
   t.end()
 })
