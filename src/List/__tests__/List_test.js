78e01ec02943e37dc46c3f408edc6c22fcc0e30a
Kevin Mees <kev.mees@gmail.com>
2016-07-12 14:29:33 +0200

    test(List): add test for selectable lists

--- src/List/__tests__/List_test.js
+++ src/List/__tests__/List_test.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { render } from 'enzyme'
 import test from 'tape'
 
-import List from '../index.js'
+import List from '..'
+import ListItem from '../../ListItem'
 
 test('List', t => {
   t.ok(List, 'export')
@@ -19,6 +20,21 @@ test('List#render - simple', t => {
   t.assert(container.is('ul.ms-List', 'container'))
   t.assert(container.is('[data-fabric="List"]'), 'data-fabric')
   t.equal(container.text(), 'Foo')
+
+  t.end()
+})
+
+test('List#render - selectable', t => {
+  // shallow test would be better
+  const wrapper = render(
+    <List selectable>
+      <ListItem primaryText="Foo" />
+      <ListItem primaryText="Bar" />
+      <ListItem primaryText="Baz" />
+    </List>
+  )
+
+  t.assert(wrapper.find('.ms-ListItem-selectionTarget').length, 3)
 
   t.end()
 })
